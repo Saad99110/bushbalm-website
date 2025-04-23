f1218c2126f9e5639533e4cba4f344d3fdbb8ea0
Saad99110 <hafizzeeshanmurtaza@gmail.com>
2025-04-23 21:12:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function addToCart() {
+    alert("Added to cart! Your lips will thank you.");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thanks for reaching out! We'll contact you soon.");
+}
